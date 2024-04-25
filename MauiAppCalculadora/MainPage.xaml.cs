18f175f0ebeedb2f709ae6a17f59e32395778266
JailtonJunior14 <jailtonjrbr@gmail.com>
2024-04-25 16:00:41 -0300

    falta coisa

--- MauiAppCalculadora/MainPage.xaml.cs
+++ MauiAppCalculadora/MainPage.xaml.cs
@@ -2,26 +2,50 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        string? operacao = null;
+        double memoria_calc_pre_operacao = 0;
+        double memoria_calc_pos_operacao = 0;
+        string aparecer_visor = "";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void remover_sinais()
+        {
+            if (aparecer_visor == "+" || 
+                aparecer_visor == "-" ||
+                aparecer_visor == "*" ||
+                aparecer_visor == "/")
+            {
+                aparecer_visor = "";
+            }
+        }
         private void zerar_Clicked(object sender, EventArgs e)
         {
+            visor.Text = "0";
+            aparecer_visor = "";
+            memoria_calc_pos_operacao = 0;
+            memoria_calc_pre_operacao = 0;
+            operacao = null;
 
         }
 
         private void maismenos_Clicked(object sender, EventArgs e)
         {
-
+            double valor = Convert.ToDouble(visor.Text);
+            valor = valor * -1;
+            visor.Text = valor.ToString();
         }
 
-        private void dividir_Clicked(object sender, EventArgs e)
+        private void porcento_Clicked(System.Object sender, System.EventArgs e)
         {
 
+        }
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+           
         }
 
         private void num7_Clicked(object sender, EventArgs e)
